6193c7f1d8969b738ad5713bf76a2bdbe4a1fed8
Jenny <jeniferfernandezares@gmail.com>
2025-04-13 15:20:29 +0200

    Add files via upload

---
+++ CheckPoint7/script.js
@@ -0,0 +1,11 @@
+function ejercicio(a, b, c, d) {
+    let sumaUno = a + b;
+    let sumaDos= c + d;
+    let total= sumaUno * sumaDos;
+    if (total > 50) {
+        console.log("¡El numero es mayor que 50!")
+    } else {
+        console.log("¡El numero es menor que 50!" )
+    }
+}
+ejercicio(4,2,6,4);
